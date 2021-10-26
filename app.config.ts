4eed939133fa59e36dd6bd492f23dfc2f67b2ae6
Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>
2021-10-26 11:14:01 +0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -10,13 +10,10 @@ import "dotenv/config";
  */
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: "example",
-  slug: "example",
+  name: config.name || "example",
+  slug: config.slug || "example",
   extra: {
     ...config.extra,
     ...process.env
-  },
-  version: "1.0.0",
-  privacy: "unlisted",
-  scheme: "fi.metatavu.example"
+  }
 });
